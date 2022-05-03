7cdd267f3c631a53b6734b819f3b4e34b2247a4e
Loris Bollengier <loris.bollengier@paris-malaquais.archi.fr>
2022-05-03 23:47:50 +0200

    added Js to scale font size based on overflow

--- calculator.js
+++ calculator.js
@@ -3,7 +3,10 @@
 
 let calculator_screen = document.getElementById("textval");
 let equal = document.getElementById("equal");
+let clear = document.getElementById("clr");
 
+let style = window.getComputedStyle(calculator_screen, null).getPropertyValue('font-size');
+let firstSize = parseFloat(style);
 
 //Function to add the value of the button clicked in the calculator screen
 function display(val){
@@ -29,3 +32,46 @@ function display(val){
      calculator_screen.innerText = "0"
  }
 
+//Function to check if text is overflowing the calculator screen
+
+function checkOverflow(el) {
+    let curOverflow = el.style.overflow ;
+    if (!curOverflow || curOverflow === "visible") {
+        el.style.overflow = "hidden"
+    }
+    let isOverflowing = el.clientWidth < el.scrollWidth || el.clientHeight < el.scrollHeight ;
+    el.style.overflow = curOverflow ;
+    console.log(isOverflowing);
+    return isOverflowing
+}
+
+//Function to adjust the font size of the text in the calculator screen
+
+const incrementionRate = 1 // To augment/diminish 0.05em every step 
+
+function adjustFontSize(el) {
+    let lastSize = parseFloat(style);
+
+    let i = 0;
+    while (checkOverflow(el)) {
+        lastSize = lastSize - incrementionRate;
+        el.style.fontSize = `${lastSize}px`
+    }
+    console.log(lastSize)
+}
+
+// Execute function on each button pressed
+
+document.querySelectorAll('input').forEach(item => {
+    item.addEventListener('click', () => {
+        adjustFontSize(calculator_screen)
+    } )
+})
+
+// Reset font size when Clr button pressed
+
+clear.addEventListener("click", function resetFontSize() {
+    calculator_screen.style.fontSize = `${firstSize}px`
+    console.log("ok")
+
+ });
